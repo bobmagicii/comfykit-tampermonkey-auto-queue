09e6941f464953fac5e36c2cb30ea1ddf6dcbaf4
bobmagicii <bob@majdak.net>
2025-03-18 21:03:20 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# comfykit-tampermonkey-auto-queue
-If the queue has been idle for a while kick off a render. Work in other tabs without wasting too much idle GPU time or being unable to queue something sooner than later. This allows for your system to cool down a bit between renders or other processes to get stuff done.
+# ComfyKit - Auto Queue Tampermonkey Script
+
+This is a script for Tampermonkey, maybe Greasemonkey too I don't know I
+don't use that one, that latches onto the ComfyUI interface to provide an
+auto queue function.
+
+When enabled it will periodically check if the queue is empty. If it has been
+empty for the time specified (30s by default) it will then click the Queue
+button for you.
+
+This was built to provide a way another tab I forgot about to still get stuff
+done when the tab I am currently working in has finished.
+
+It also allows some time for my laptop to cool off before running another job
+so I can "run it all day" without running my GPU at 100% for 24 hours straight.
---
+++ comfykit-tampermonkey-auto-queue.js
@@ -0,0 +1,320 @@
+// ==UserScript==
+// @name         ComfyKit - Auto Queue Tool
+// @namespace    bobmagicii
+// @author       bobmagicii
+// @version      1.0.0
+// @description  If the queue has been idle for a while kick off a render. Work in other tabs without wasting too much idle GPU time or being unable to queue something sooner than later. This allows for your system to cool down a bit between renders or other processes to get stuff done.
+// @include      /https?:\/\/(.+?):8188\/(.*?)/
+// @grant        none
+// ==/UserScript==
+
+/*//
+if you are not getting the Auto Queue button there are two main things that
+could be going on:
+
+	* the @include above needs to be fixed for your port number.
+	* the selector properties below need to be fixed for updated comfyui.
+
+comfyui has been updating their ui frequently and i do not update mine very
+frequently at all. because i enjoy my things working when needed. plus sampler
+stuff seems to constantly change which can cause rendering differences in the
+middle of projects. hopefully they do not change the ui too much but in the
+event the console claims the boot failed then the selector properties can be
+updated to try and catch however it has changed today.
+
+last comfy version i personally updated it for:
+	* Git: 65a865918277b9413571c00fa402c5ff0a224225
+	* Date 2024-10-29
+
+//*/
+
+'use strict';
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+class QueueMgr {
+
+	////////////////////////////////
+	// CONFIG //////////////////////
+
+	// this may need tweaked constantly with how fast comfy is iterating.
+
+	selectorMenubar  = '.comfyui-body-top .comfyui-menu-right > .flex';
+	selectorQueueBtn = 'button[aria-label="Queue"]';
+	selectorQueueNum = 'button[aria-label="Queue (q)"] .p-badge';
+
+	// tampermonkey is way faster than the comfyui front end so we need to sit
+	// and spin a bit.
+
+	bootAttemptMax   = 10;
+	bootAttemptDelay = 2.0;
+
+	// and these are the default queue values. check time is how frequently
+	// it checks. while idle time is how long it must have witnessed the queue
+	// idle before going to give your system some cooldown time or let some
+	// other processes have their way with your hardware.
+
+	queueCheckTime   = 10.0;
+	queueIdleTime    = 30.0;
+
+	////////////////////////////////
+	// NOT CONFIG //////////////////
+
+	bootAttempt = null;
+	elMenuBar   = null;
+	elQueueBtn  = null;
+	elQueueNum  = null;
+	elToggleBtn = null;
+	elInputFreq = null;
+	elInputIdle = null;
+	timerBoot   = null;
+	timerQueue  = null;
+	numInQueue  = null;
+	idleTime    = null;
+	enabled     = false;
+
+	////////////////////////////////
+	////////////////////////////////
+
+	constructor() {
+
+		this.bootAttempt = 0;
+		this.tryToBootup();
+
+		return;
+	};
+
+	tryToBootup() {
+
+		let found = false;
+
+		////////
+
+		this.bootAttempt += 1;
+
+		if(this.bootAttempt > this.bootAttemptMax)
+		throw new Exception(`[QueueMgr.tryToBootup] bailing after ${this.bootAttemptMax} failed attempts.`);
+
+		console.log(`[QueueMgr.tryToBootup] attempt ${this.bootAttempt}.`);
+
+		////////
+
+		this.elMenuBar = document.querySelector(this.selectorMenubar);
+		this.elQueueBtn = document.querySelector(this.selectorQueueBtn);
+
+		console.log(`[QueueMgr.tryToBootup] menu bar = ${this.elMenuBar}`);
+		console.log(`[QueueMgr.tryToBootup] queue btn = ${this.elQueueBtn}`);
+
+		////////
+
+		found = (true
+			 && (this.elMenuBar !== null)
+			 && (this.elQueueBtn !== null)
+		);
+
+		if(!found) {
+			setTimeout(
+				(this.tryToBootup.bind(this)),
+				(this.bootAttemptDelay * 1000)
+			);
+
+			return;
+		}
+
+		this.addElementsToUI();
+
+		return;
+	};
+
+	addElementsToUI() {
+
+		let btnGrp = null;
+
+		////////
+
+		this.elToggleBtn = document.createElement('button');
+		this.elToggleBtn.classList.add('comfyui-button');
+		this.elToggleBtn.addEventListener('click', this.onClickToggleBtn.bind(this));
+		this.turnAutoQueueOff();
+
+		btnGrp = document.createElement('div');
+		btnGrp.classList.add('comfyui-button-group');
+		btnGrp.append(this.elToggleBtn);
+
+		this.elMenuBar.prepend(btnGrp);
+
+		return;
+	};
+
+	////////////////////////////////
+	////////////////////////////////
+
+	tryToQueue() {
+
+		let qnum = this.findQueueNumElement();
+
+		////////
+
+		if(qnum === null)
+		this.updateQueueInfo(0);
+
+		else
+		this.updateQueueInfo(parseInt(qnum.innerText));
+
+		console.log(`[QueueMgr.tryToQueue] queue size = ${this.numInQueue}.`);
+
+		////////
+
+		if(this.isIdleLong()) {
+			console.log(`[QueueMgr.tryToQueue] adding to queue.`);
+			this.elQueueBtn.click();
+		}
+
+		////////
+
+		if(this.isAutoQueueOn())
+		this.timerQueue = setTimeout(
+			(this.tryToQueue.bind(this)),
+			(this.queueCheckTime * 1000)
+		);
+
+		return;
+	};
+
+	turnAutoQueueOn() {
+
+		this.enabled = true;
+		this.elToggleBtn.innerText = 'AQ: ON';
+
+		this.tryToQueue();
+
+		return;
+	};
+
+	turnAutoQueueOff() {
+
+		this.enabled = false;
+		this.elToggleBtn.innerText = 'AQ: Off';
+
+		if(this.timerQueue !== null) {
+			clearInterval(this.timerQueue);
+			this.timerQueue = null;
+		}
+
+		return;
+	};
+
+	isAutoQueueOn() {
+
+		return this.enabled;
+	};
+
+	isIdle() {
+
+		return (this.numInQueue === 0);
+	};
+
+	isIdleLong() {
+
+		if(!this.isIdle())
+		return false;
+
+		if(this.getTimeSinceIdle() < this.queueIdleTime)
+		return false;
+
+		return true;
+	};
+
+	getTimeSinceIdle() {
+
+		let val = (this.getTimestampNow() - this.idleTime);
+
+		console.log(`[QueueMgr.getTimeSinceIdle] queue has been idle for ${val} sec.`);
+
+		return val;
+	};
+
+	getTimestampNow() {
+
+		return Math.floor(Date.now() / 1000);
+	};
+
+	findQueueNumElement() {
+
+		// this element wont exist if the queue size is 0 so it will be
+		// returning null in that case.
+
+		return document.querySelector(this.selectorQueueNum);
+	}
+
+	updateQueueInfo(numInQueue) {
+
+		// if the queue is empty but was not empty last time then
+		// we need to update the timestamp of when it was last idled.
+
+		if((numInQueue === 0) && (this.numInQueue !== 0))
+		this.idleTime = this.getTimestampNow();
+
+		////////
+
+		this.numInQueue = numInQueue;
+
+		return;
+	};
+
+	////////////////////////////////
+	////////////////////////////////
+
+	onClickToggleBtn() {
+
+		////////
+
+		if(this.isAutoQueueOn())
+		this.turnAutoQueueOff();
+
+		else
+		this.turnAutoQueueOn();
+
+		////////
+
+		console.log(`[QueueMgr.onClickToggleBtn] enabled = ${(this.enabled ? 'true' : 'false')}`);
+
+		return false;
+	};
+
+	////////////////////////////////
+	////////////////////////////////
+
+	static OnReady() {
+		console.log('[QueueMgr.OnReady] OK');
+
+		new QueueMgr;
+
+		return;
+	};
+
+	static WhenDocumentReady(){
+
+		if(document.readyState != 'loading') {
+			QueueMgr.OnReady();
+			return;
+		}
+
+		document.addEventListener(
+			'DOMContentLoaded',
+			QueueMgr.OnReady
+		);
+
+		return;
+	};
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+(function() {
+	QueueMgr.WhenDocumentReady();
+	return;
+})();
